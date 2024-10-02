96dbce5e14afe9acadc40e147398e8cc8729df09
Lubna Ali <LubnaAliiAdel@gmail.com>
2024-10-02 11:26:49 +0300

    Refactor code

--- AuthenticationApp/Controller/ProfileVC.swift
+++ AuthenticationApp/Controller/ProfileVC.swift
@@ -12,17 +12,16 @@ class ProfileVC: UIViewController {
     @IBOutlet private weak var tableView: UITableView!
     @IBOutlet weak var logOutButton: UIButton!
     
-    let usersData: [UserData] = [
-        .init(title: "Name", value: UserDefaults.standard.string(forKey: "name")),
-        .init(title: "Email", value: UserDefaults.standard.string(forKey: "email")),
-        .init(title: "Gender", value: UserDefaults.standard.string(forKey: "gender")),
+    private let uiModels: [ProfileCellUIModel] = [
+        .init(title: "Name", value: UserDefaults.standard.string(forKey: "name") ?? ""),
+        .init(title: "Email", value: UserDefaults.standard.string(forKey: "email") ?? ""),
+        .init(title: "Gender", value: UserDefaults.standard.string(forKey: "gender") ?? ""),
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        setUserData()
-        tableView.delegate = self
+        setProfileImage()
         tableView.dataSource = self
     }
     
@@ -36,10 +35,6 @@ extension ProfileVC {
     func setupUI() {
         title = "Profile"
         logOutButton.setTitle("Log out", for: .normal)
-    }
-    
-    func setUserData() {
-        setProfileImage()
     }
     
     func setProfileImage() {
@@ -56,20 +51,15 @@ extension ProfileVC {
 
 extension ProfileVC: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        usersData.count
+        uiModels.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: .subtitle, reuseIdentifier: nil)
-        cell.textLabel?.text = usersData[indexPath.row].title
-        cell.detailTextLabel?.text = usersData[indexPath.row].value
+        cell.selectionStyle = .none
+        cell.backgroundColor = .tertiarySystemGroupedBackground
+        cell.textLabel?.text = uiModels[indexPath.row].title
+        cell.detailTextLabel?.text = uiModels[indexPath.row].value
         return cell
     }
 }
-
-extension ProfileVC: UITableViewDelegate {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print(indexPath)
-    }
-}
-
