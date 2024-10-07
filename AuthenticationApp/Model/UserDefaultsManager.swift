2e31be44c8c53d9d7a719f1fab7d23b4f2a187f6
Lubna Ali <LubnaAliiAdel@gmail.com>
2024-10-07 15:27:27 +0300

    Add enum UserDefaultsKeys instead of Strings

--- AuthenticationApp/Model/UserDefaultsManager.swift
+++ AuthenticationApp/Model/UserDefaultsManager.swift
@@ -13,54 +13,63 @@ class UserDefaultsManager {
     
     private init() {}
     
+    // MARK: - UserDefaultsKeys
+    private enum UserDefaultsKeys: String {
+        case name
+        case email
+        case password
+        case gender
+        case profileImage
+        case isLoggedIn
+    }
+    
     // MARK: - Setters
     func setName(_ name: String) {
-        UserDefaults.standard.set(name, forKey: "name")
+        UserDefaults.standard.set(name, forKey: UserDefaultsKeys.name.rawValue)
     }
     
     func setEmail(_ email: String) {
-        UserDefaults.standard.set(email, forKey: "email")
+        UserDefaults.standard.set(email, forKey: UserDefaultsKeys.email.rawValue)
     }
     
     func setPassword(_ password: String) {
-        UserDefaults.standard.set(password, forKey: "password")
+        UserDefaults.standard.set(password, forKey: UserDefaultsKeys.password.rawValue)
     }
     
     func setGender(_ gender: String) {
-        UserDefaults.standard.set(gender, forKey: "gender")
+        UserDefaults.standard.set(gender, forKey: UserDefaultsKeys.gender.rawValue)
     }
     
     func setProfileImage(_ profileImageData: Data) {
-        UserDefaults.standard.set(profileImageData, forKey: "profileImage")
+        UserDefaults.standard.set(profileImageData, forKey: UserDefaultsKeys.profileImage.rawValue)
     }
     
-    func setIsLoggedIn(_ isLoggedIn: Bool ) {
-        UserDefaults.standard.set(true, forKey: "isLoggedIn")
+    func setLoginStatus(_ isLoggedIn: Bool ) {
+        UserDefaults.standard.set(true, forKey: UserDefaultsKeys.isLoggedIn.rawValue)
     }
-   
     
     // MARK: - Getters
     func getName() -> String {
-        UserDefaults.standard.string(forKey: "name") ?? ""
+        UserDefaults.standard.string(forKey: UserDefaultsKeys.name.rawValue) ?? ""
     }
     
     func getEmail() -> String {
-        UserDefaults.standard.string(forKey: "email") ?? ""
+        UserDefaults.standard.string(forKey: UserDefaultsKeys.email.rawValue) ?? ""
     }
     
     func getPassword() -> String {
-        UserDefaults.standard.string(forKey: "password") ?? ""
+        UserDefaults.standard.string(forKey: UserDefaultsKeys.password.rawValue) ?? ""
     }
     
     func getGender() -> String {
-        UserDefaults.standard.string(forKey: "gender") ?? ""
+        UserDefaults.standard.string(forKey: UserDefaultsKeys.gender.rawValue) ?? ""
     }
-        
+    
     func getProfileImage() -> Data? {
-        UserDefaults.standard.data(forKey: "profileImage")
+        UserDefaults.standard.data(forKey: UserDefaultsKeys.profileImage.rawValue)
     }
     
     func getLoginStatus() -> Bool {
-        UserDefaults.standard.bool(forKey: "isLoggedIn")
+        UserDefaults.standard.bool(forKey: UserDefaultsKeys.isLoggedIn.rawValue)
     }
 }
